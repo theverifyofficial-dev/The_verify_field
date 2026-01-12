bfc598afc7cfec5df1da8b76df990aa19ba89f19
Lokesh <lokesh14may99@gmail.com>
2026-01-12 21:25:06 +0530

    12/Jan-- By Lokesh

--- lib/Gemini_AI/Demo_chat.dart
+++ lib/Gemini_AI/Demo_chat.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
+// import 'package:google_generative_ai/google_generative_ai.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 

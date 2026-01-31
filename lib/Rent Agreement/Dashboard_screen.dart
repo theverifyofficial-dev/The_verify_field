87973dd17b0506e7e9fdecd76ad8a00ef62c025d
The Verify <theverifyofficial@gmail.com>
2026-01-31 16:46:38 +0530

    31/Jan -- By Gurdeep office

--- lib/Rent Agreement/Dashboard_screen.dart
+++ lib/Rent Agreement/Dashboard_screen.dart
@@ -184,7 +184,8 @@ class _AgreementDashboardState extends State<AgreementDashboard> {
           },
             _rewardStatus?.isDiscounted == true,
 
-          ),        ],
+          ),
+                                                                                ],
       ),
             if (_rewardStatus?.isDiscounted == true)
               Align(

484e5d9bf352ba6927e5a346ba2aedc5ce0b2bef
Verifty <gurdeep.singh136394@gmail.com>
2026-04-10 11:07:35 +0530

    10/April -- By Gurdeep

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,12 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        OcrPlugin.register(with: registrar(forPlugin: "OcrPlugin")!)  // ← SIRF YEH LINE ADD KARO
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
